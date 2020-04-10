ceafd88603ad67fc2214ea835e00d23e31bb8067
John Taylor <johtaylo@microsoft.com>
2020-04-09 20:39:34 -0700

    export new functions

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const graph = require('./graph');
 exports.Graph = graph.Graph;
 exports.literal = graph.literal;
 exports.label = graph.label;
+exports.serialize = graph.serialize;
+exports.deserialize = graph.deserialize;
